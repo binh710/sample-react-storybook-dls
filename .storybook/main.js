be7cffbbcb9c1edfb96c255c713217dd38d04063
Binh <thanhbinh710@gmail.com>
2022-04-19 15:32:55 +0800

    Add scss loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-scss"
   ],
   "framework": "@storybook/react",
   "core": {
